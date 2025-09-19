0176564b474410569181dde178cc39cb0bacc3da
francissprog <francissprog@gmail.com>
2025-09-19 21:17:15 +0800

    remove unecessary code

--- db/config.php
+++ db/config.php
@@ -1,7 +1,5 @@
 <?php
 $servername = "127.0.0.1";  // or "127.0.0.1"
-$dbname = "josephine";  // change to your DB name
-
 $connection  = new mysqli($servername, "root", "", "josephine");
 
 //check connection
