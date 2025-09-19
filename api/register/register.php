d077554e9d0b76869e111bb5571c3170cbedfe25
francissprog <francissprog@gmail.com>
2025-09-19 10:52:52 +0800

    asdsf

--- api/register/register.php
+++ api/register/register.php
@@ -12,15 +12,38 @@ $email = $data['email'];
 $password = $data['password'];
 
 // connect to the database
-require_once __DIR__ . "../../../db/config.php";
+require_once("../../db/config.php");
 
-// $result = 
+
+
 // validate the data
 if (!isset($username) || !isset($email) || !isset($password)) {
     echo json_encode([        
-        "error" => "Username, email and password are required."
+        "message" => "Username, email and password are required."
     ]);
+    http_response_code(400);
     exit();
+}
+
+// check if user already exist
+$stmt = $connection->prepare("SELECT id FROM users WHERE username = ? OR email = ?");
+$stmt->bind_param("ss", $username, $email);
+$stmt->execute();
+
+// get the result
+$result = $stmt->get_result();
+
+
+// validate the result 
+if ($result->num_rows > 0){
+    echo json_encode([
+        "message" => "Username or email already exists."
+    ]);
+    http_response_code(409);
+    exit();
+
+    http_response_code();
+
 }
 
 
