832be7014cdacd1b77226ddb8f85319ac829d43b
Alvin-progg <doctoralvin726@gmail.com>
2025-10-04 15:12:19 +0800

    Set up

---
+++ api/payment/insertPayment.php
@@ -0,0 +1,39 @@
+<?php
+header("Content-Type: application/json");
+session_start();
+
+require_once("../../db/config.php");
+$input = file_get_contents("php://input");
+$data = json_decode($input, true);
+
+// validate cart
+if (!isset($data['cart']) || empty($data['cart'])) {
+  http_response_code(400);
+  echo json_encode(["message" => "Cart is empty"]);
+  exit();
+}
+
+$cart = $data['cart'];
+$userId = $_SESSION['user_id'] ;
+$customize = $data['customize'] ?? null;
+
+// prepare statement
+$stmt = $connection->prepare("
+  INSERT INTO orderDetails(user_id, product_name, quantity, price, customize) 
+  VALUES (?, ?, ?, ?, ?)
+");
+
+foreach ($cart as $item) {
+  $name     = $item['name'];
+  $price    = (float)$item['price'];
+  $quantity = (int)$item['quantity'];
+  $stmt->bind_param("isids", $userId, $name, $quantity, $price, $customize);
+  $stmt->execute();
+
+}
+$stmt->close();
+
+echo json_encode([
+  "message" => "Order successfully placed",
+  "items_count" => count($cart)
+]);
